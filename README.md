4347ebcf58b78b568b4c0b2926be85ae40d456ac
louisamurray <121989665+louisamurray@users.noreply.github.com>
2023-02-23 02:04:51 +1300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# OMScope
+# Task Scope Checker
+The Task Scope Checker is a web-based tool for determining if a given task falls within the scope of the Office Manager role at the Marlborough Chamber of Commerce.
+
+## Table of Contents
+- Background
+- Installation
+- Usage
+- Contributing
+
+## Background
+At the Marlborough Chamber of Commerce, the Office Manager is responsible for a variety of tasks across several areas of responsibility, including office operations, finance, marketing, IT, and more. The Task Scope Checker was developed to help ensure that tasks are properly assigned to the correct person based on their area of responsibility.
+
+The tool works by allowing users to input a brief description of the task they need help with. The system then checks the task against a list of keywords associated with various areas of responsibility, and provides the user with guidance on which area of responsibility the task may fall under. If the task falls within the Office Manager's scope of responsibility, the user is prompted to provide additional details about the task.
+
+## Installation
+To install the Task Scope Checker, simply download the files and place them in a directory on your web server. There are no additional dependencies or setup required.
+
+## Usage
+To use the Task Scope Checker, simply navigate to the URL where the tool is installed, and enter a brief description of the task you need help with. The system will automatically check the task against a list of keywords associated with various areas of responsibility, and provide guidance on which area of responsibility the task may fall under.
+
+If the task falls within the Office Manager's scope of responsibility, the user will be prompted to provide additional details about the task, including the requester's name, priority level, due date, and specific instructions. Once the details are entered, the system will automatically generate an email to the appropriate person with all the necessary information.
+
+## Contributing
+Contributions to the Task Scope Checker are welcome and encouraged. To contribute, simply fork the repository, make your changes, and submit a pull request. Before making significant changes, it is recommended that you first open an issue to discuss your proposed changes with the maintainers.
---
+++ js/app.js
@@ -0,0 +1,79 @@
+// Define the areas of responsibility and their associated keywords
+var areasOfResponsibility = {
+    "Objectives": ["objectives"],
+    "Key Deliverables": ["deliverables"],
+    "KPI's": ["KPI", "key performance indicator"],
+    "Office Operations": ["front desk", "receptionist", "office procedure", "supplies", "equipment", "policies"],
+    "Daily Management of Chamber Office": ["office management"],
+    "Export Certification": ["certificate", "export documents", "export", "import", "COO", "COF", "CFS", "certificate", "Certificate of Origin", "cert"],
+    "Accounts and Finance": ["pay","payment","accounts", "receivable", "payable", "invoice", "GST", "creditors", "bills"],
+    "Marketing & Graphic Design": ["branding", "social media", "competitors", "marketing campaigns", "promotional material"],
+    "Computers & IT": ["IT processes", "hardware", "software", "troubleshooting", "website"],
+    "Membership": ["induction", "member interactions", "CRM", "membership renewal"],
+    "Chamber Events": ["event logistics", "technical equipment", "representing the Chamber"],
+    "Health & Safety Officer": ["Health & Safety", "compliance", "risk"],
+    "Facilities Management": ["building maintenance", "janitorial services", "property management", "groundskeeping"],
+    "Human Resources": ["recruiting", "onboarding", "employee relations", "payroll", "benefits administration", "performance management"],
+    "Project Management": ["planning", "budgeting", "scheduling", "coordination", "stakeholder management"],
+    "Procurement": ["sourcing", "vendor management", "contract negotiation", "inventory management"]
+  };
+  
+  function checkScope() {
+    // Get the input value
+    var task = document.getElementById("taskInput").value;
+  
+    // Check if the task matches an area of responsibility
+    var taskMatches = false;
+    var matchedArea = null;
+    for (var area in areasOfResponsibility) {
+      var keywords = areasOfResponsibility[area];
+      for (var i = 0; i < keywords.length; i++) {
+        if (task.toLowerCase().includes(keywords[i].toLowerCase())) {
+          taskMatches = true;
+          matchedArea = area;
+          break;
+        }
+      }
+      if (taskMatches) {
+        break;
+      }
+    }
+  
+  
+    // Display the form for providing additional details
+    if (taskMatches) {
+      document.getElementById("result").innerHTML = "The requested task may fall within the scope of the Office Manager role (" + matchedArea + "). Please provide additional details:";
+      document.getElementById("detailsForm").style.display = "block";
+      document.getElementById("matchedArea").value = matchedArea;
+    } else {
+      document.getElementById("result").innerHTML = "The requested task may not fall within the scope of the Office Manager role. Please contact the CEO for clarification";
+      document.getElementById("detailsForm").style.display = "none";
+    }
+  }
+
+  function submitDetails() {
+    // Get the form field values
+    var requester = document.getElementById("requester").value;
+    var priority = document.querySelector('input[name="priority"]:checked').value;
+    var dueDate = document.getElementById("dueDate").value;
+    var instructions = document.getElementById("instructions").value;
+    var taskInput = document.getElementById("taskInput").value;
+    var matchedArea = document.getElementById("matchedArea").value;
+  
+    // Format the email body
+    var emailBody = "Requester: " + requester + "\n" +
+                    "Priority: " + priority + "\n" +
+                    "Due Date: " + dueDate + "\n" +
+                    "Instructions: " + instructions + "\n" +
+                    "Task Description: " + taskInput + "\n\n" +
+                    "The task '" + taskInput + "' is associated with the following area of responsibility: " + matchedArea;
+  
+    // Open the user's default email client with the pre-populated email fields
+    var emailLink = 'mailto:info@marlboroughchamber.nz?subject=New task request&body=' + encodeURIComponent(emailBody);
+    window.location.href = emailLink;
+  
+    // Reset the form and hide it
+    document.getElementById("detailsForm").reset();
+    document.getElementById("detailsForm").style.display = "none";
+  }
+  
